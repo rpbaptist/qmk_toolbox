3580ddb1b75256105f1e5d4e07e97e0d5c7263f5
Richard Baptist <rpbaptist@gmail.com>
2018-10-08 09:41:24 +0200

    Update

--- keyboards/iris/keymaps/rpbaptist/config.h
+++ keyboards/iris/keymaps/rpbaptist/config.h
@@ -34,5 +34,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
-#define TAPPING_TERM 110
-#define IGNORE_MOD_TAP_INTERRUPT
+#define TAPPING_TERM 100
+#define PERMISSIVE_HOLD
+// #define IGNORE_MOD_TAP_INTERRUPT
