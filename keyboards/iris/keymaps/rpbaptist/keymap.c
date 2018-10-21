bb0ceeb2b07dac89e539a0ef9c273c66375cf7fe
Richard Baptist <rpbaptist@gmail.com>
2018-10-21 13:19:49 +0200

    Use two keys on left pad

--- keyboards/iris/keymaps/rpbaptist/keymap.c
+++ keyboards/iris/keymaps/rpbaptist/keymap.c
@@ -36,9 +36,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      LOWER,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     LCTLBR,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SPC,           RAISENT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RCTLBR,
+     LCTLBR,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SPC,           KC_ENT,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RCTLBR,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    KC_LALT, KC_LSFT, KC_SPC,                    RAISENT, KC_RSFT, KC_RGUI
+                                    KC_LALT, KC_LSFT, KC_SPC,                    RAISE,   KC_RSFT, KC_RGUI
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
