3f39b25632a79cdf8b8144edab5f55caed3772d4
Richard Baptist <rpbaptist@gmail.com>
2018-11-15 14:06:53 +0100

    Disable unused features

--- keyboards/iris/keymaps/rpbaptist/config.h
+++ keyboards/iris/keymaps/rpbaptist/config.h
@@ -28,6 +28,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
+#define NO_ACTION_ONESHOT
+#define NO_ACTION_MACRO
+
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
 #define RGBLED_NUM 12
