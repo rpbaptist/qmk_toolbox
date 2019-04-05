22edaa01c4e347ae8c5a8fe1072e32b8c687afe4
Richard Baptist <rpbaptist@gmail.com>
2019-04-05 11:45:04 +0200

    Increase tapping term

--- keyboards/iris/keymaps/rpbaptist/config.h
+++ keyboards/iris/keymaps/rpbaptist/config.h
@@ -37,7 +37,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 16
 #define RGBLIGHT_VAL_STEP 16
-#define TAPPING_TERM 110
+#define TAPPING_TERM 120
 #define PERMISSIVE_HOLD
 // #define IGNORE_MOD_TAP_INTERRUPT
 
