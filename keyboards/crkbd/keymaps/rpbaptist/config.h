0d06ef100828fd617ce3fc0b98bb332dae48555a
Richard Baptist <rpbaptist@gmail.com>
2019-04-23 14:34:22 +0200

    Update rules

--- keyboards/crkbd/keymaps/rpbaptist/config.h
+++ keyboards/crkbd/keymaps/rpbaptist/config.h
@@ -38,9 +38,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 16
 #define RGBLIGHT_VAL_STEP 16
+
 #define TAPPING_TERM 90
 #define PERMISSIVE_HOLD
-// #define IGNORE_MOD_TAP_INTERRUPT
+#define RETRO_TAPPING
+#define IGNORE_MOD_TAP_INTERRUPT
 
 #define TAPPING_TOGGLE 2
 
